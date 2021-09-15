62c2fb6922753e58bcecb853eafd3c22931a8001
Pozitrone <36227512+Pozitrone@users.noreply.github.com>
2021-09-15 17:33:16 +0200

    Initial commit

---
+++ Assets/Scripts/UnitTests/ExampleTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using Runtime;
+
+namespace UnitTests
+{
+    public class ExampleTests
+    {
+        // Use [Test] for basic testing
+        // Use [UnityTest] for IEnumerators
+
+        [Test]
+        public void TestMethod()
+        {
+            int x = 27;
+            int y = Random.Range(26, 28);
+            
+            Assert.That(x == y, "X != Y"); // Make sure that x == y, otherwise return a message that X != Y
+            Assert.Fail();
+        }
+    }
+}
